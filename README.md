6d1bb7082a5265d68604d0921498eeb0321a57fb
Mooglefrooglian <mooglefrooglian@gmail.com>
2012-09-28 15:58:14 -0500

    Directory structure for all!

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Ascendant
 =========
+A game of space and combat, by Fel and Moogle.
+
+Written in Python.
---
+++ src/main.py
@@ -0,0 +1,3 @@
+print "Hello, world! I am a completely finished game."
+raw_input(">")
+print "You have been pwned by a grue."
