fc9950124e79fa915810c5a91c3575a40d792475
Mooglefrooglian <mooglefrooglian@gmail.com>
2012-10-20 20:42:04 -0500

    Minor restructuring for the ui package in preparation for UI elements.

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -9,9 +9,9 @@ game.graphics.uis = {}
 
 #Find all the UIs and load 'em up. One suspects a generic plugin system might simplify this?
 #Also I'm not using glob BECAUSE IM NOT.
-for path,dirs,files in os.walk("src/ui"): #NO I HAVE NO IDEA HOW PATHS WORK
+for path,dirs,files in os.walk("src/ui/uis"): #NO I HAVE NO IDEA HOW PATHS WORK
 	for f in files:
-		if f.endswith(".py") and not f.startswith('__init__') and not f.startswith('ui.py'):
+		if f.endswith(".py"):
 			name = re.sub(r'\.py$', '', f)
 			print("Found UI " + name)
 			u = imp.load_source(name, path + "/" + f) #We found a UI!
